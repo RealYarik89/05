621c4ea7c819fbe04c873f640eeef917326357a4
RealYarik89 <yarok.89@gmail.com>
2024-02-16 00:59:40 +0300

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+let menuButton = document.querySelector(".menu")
+
+menuButton.addEventListener("click", () =>{
+    let nav = document.querySelector(".nav")
+    nav.classList.toggle("display-none") 
+})
